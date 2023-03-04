313cecae6a6137b49459928b27eb5e49713b32b0
Lithium@87 <cibs@abv.bg>
2023-03-04 19:42:28 +0200

    Implement piloting the ship

--- .history/alien_invasion_20230304192155.py
+++ .history/alien_invasion_20230304192155.py
@@ -26,7 +26,6 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -37,14 +36,8 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
