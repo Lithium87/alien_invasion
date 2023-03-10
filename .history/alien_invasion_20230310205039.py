1c975c760c11c893de1b55e1789ee05f7a42e2e2
Lithium@87 <cibs@abv.bg>
2023-03-10 21:11:34 +0200

    Create a fleet of aliens

--- .history/alien_invasion_20230310205039.py
+++ .history/alien_invasion_20230310205039.py
@@ -85,26 +85,20 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
-        # Spacing between aliens is one alien width and one alien height.
+        # Spacing between aliens is one alien width.
         alien = Alien(self)
-        alien_width, alien_height = alien.rect.size
+        alien_width = alien.rect.width
 
-        current_x, current_y = alien_width, alien_height
-        while current_y < (self.settings.screen_height - 3 * alien_height):
-            while current_x < (self.settings.screen_width - 2 * alien_width):
-                self._create_alien(current_x, current_y)
-                current_x += 2 * alien_width
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            self._create_alien(current_x)
+            current_x += 2 * alien_width
 
-            # Finished a row; reset x, and increment y value.
-            current_x = alien_width
-            current_y += 2 * alien_height
-
-    def _create_alien(self, x_position, y_position):
+    def _create_alien(self, x_position):
         """Create an alien and place it in a row."""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
-        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
     def _update_screen(self):
